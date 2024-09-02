fb9e228e854cd041b7516436f90b231aeebcc2ad
Fadi Nasser <nassar@zenhr.com>
2024-09-02 12:42:17 +0300

    Adding the relations to the model files

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,6 @@
 class Branch < ApplicationRecord
   validates :name , presence:true 
   belongs_to :company
+  has_many :employees, dependent :destroy
+  has_one :attendance_setup, dependent: destroy
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,7 @@ class Employee < ApplicationRecord
   validates :name , hire-date: , presence:true 
   validates :email , employment-number , presence:true , uniqueness:true 
   belongs_to :branch
+  has_many :financial_packages
+  has_one :termination, dependent:destroy
+  has_and_belongs_to_many :rooms 
 end
