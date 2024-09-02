6710a0cc2c6db0287eeb08b590994ece1d8e74c1
Fadi Nasser <nassar@zenhr.com>
2024-09-02 14:44:43 +0300

    Fixing the dashes to be underscrore in the hiredate and the employmentnumber in the employee model file

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ApplicationRecord
-  validates :name , :hire-date , presence:true 
-  validates :email , :employment-number , presence:true , uniqueness:true 
+  validates :name , :hire_date , presence:true 
+  validates :email , :employment_number , presence:true , uniqueness:true 
   belongs_to :branch
   has_many :financial_packages
   has_one :termination, dependent: :destroy
