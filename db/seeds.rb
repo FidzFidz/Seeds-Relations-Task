9752175e5c19af118914247feb50279420dad738
Fadi Nasser <nassar@zenhr.com>
2024-09-02 14:40:44 +0300

    Fixing some issues in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,21 +17,21 @@ amman_branch = zenhr.branches.create!(name: "Amman Branch")
 dubai_branch = zenhr.branches.create!(name: "Dubai Branch")
 
 #Creating 3 employees for Amman Branch
-employee_1_amman = amman.branch.employees.create!(name: "Fadi Nasser", email: "nassar+1@zenhr.com", hire_date: "2022-01-01", employment_number: "1")
-employee_2_amman = amman.branch.employees.create!(name: "Malik Nimri", email: "nassar+2@zenhr.com", hire_date: "2022-01-01", employment_number: "2")
-employee_terminated_amman = amman.branch.employees.create!(name: "Ramzi Haddad", email: "nassar+3@zenhr.com", hire_date: "2022-01-01", employment_number: "3")
+employee_1_amman = amman_branch.employees.create!(name: "Fadi Nasser", email: "nassar+1@zenhr.com", hire_date: "2022-01-01", employment_number: "1")
+employee_2_amman = amman_branch.employees.create!(name: "Malik Nimri", email: "nassar+2@zenhr.com", hire_date: "2022-01-01", employment_number: "2")
+employee_terminated_amman = amman_branch.employees.create!(name: "Ramzi Haddad", email: "nassar+3@zenhr.com", hire_date: "2022-01-01", employment_number: "3")
 
 #Creating 3 employees for Dubai Branch
-employee_1_dubai = dubai.branch.employees.create!(name: "Hunny Sabbagh", email: "nassar+4@zenhr.com", hire_date: "2022-01-01", employment_number: "4")
-employee_2_dubai = dubai.branch.employees.create!(name: "Qusai Andrawes", email: "nassar+5@zenhr.com", hire_date: "2022-01-01", employment_number: "5")
-employee_2_dubai = dubai.branch.employees.create!(name: "Saif Ammari", email: "nassar+6@zenhr.com", hire_date: "2022-01-01", employment_number: "6")
+employee_1_dubai = dubai_branch.employees.create!(name: "Hunny Sabbagh", email: "nassar+4@zenhr.com", hire_date: "2022-01-01", employment_number: "4")
+employee_2_dubai = dubai_branch.employees.create!(name: "Qusai Andrawes", email: "nassar+5@zenhr.com", hire_date: "2022-01-01", employment_number: "5")
+employee_3_dubai = dubai_branch.employees.create!(name: "Saif Ammari", email: "nassar+6@zenhr.com", hire_date: "2022-01-01", employment_number: "6")
 
 #Terminate employee: Employee Terminated Amman as of: 31-Dec-2024
-Termination.create!(date: "2024-31-12" , employee: employee_terminated_amman)
+Termination.create!(date: "2024-12-31" , employee: employee_terminated_amman)
 
 #Creating employee_1 with 2 financial packages
 employee_1_amman.financial_packages.create!(from_date "2023-01-01", to_date: "2023-12-31", amount: 500)
-employee_1_amman.financial_packages.create!(from_date "01-01-2024", to_date: "2024-12-31" , amount: 600)
+employee_1_amman.financial_packages.create!(from_date "2024-01-01", to_date: "2024-12-31" , amount: 600)
 
 #Creating an employee_2 with 2 financial packages
 employee_2_amman.financial_packages.create!(from_date "2023-01-01", to_date: "2023-12-31", amount: 500)
